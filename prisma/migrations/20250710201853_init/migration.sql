f500377deaf3a2b400e32b73e8cabe8dc0b57fda
Ayush Patel <ay.patel077@gmail.com>
2025-07-17 23:44:14 -0500

    campaign sql

--- prisma/migrations/20250710201853_init/migration.sql
+++ prisma/migrations/20250710201853_init/migration.sql
@@ -22,3 +22,21 @@ CREATE TABLE `User` (
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Campaign` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `category` VARCHAR(255) NOT NULL,
+    `shortDescription` TEXT NOT NULL,
+    `fullDescription` TEXT NOT NULL,
+    `fundingGoal` INT NOT NULL,
+    `currentFunding` INT NOT NULL DEFAULT 0,
+    `support` VARCHAR(255) NOT NULL,
+    `votes` VARCHAR(255) NOT NULL,
+    `deadline` DATETIME NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+    `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
